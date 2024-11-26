e2e5d1936aca17c559cc35e514037f5816855a3d
Evlin Sitanggang <105533381+evlinzxxx@users.noreply.github.com>
2024-11-26 18:23:25 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,60 +1,111 @@
 import streamlit as st
+import pandas as pd
 import joblib
-import numpy as np
+from data_preprocessing import data_preprocessing, encoder_Course, encoder_Daytime_evening_attendance, encoder_Displaced,encoder_Fathers_occupation, encoder_International, encoder_Mothers_occupation, encoder_Scholarship_holder, encoder_Tuition_fees_up_to_date
+from prediction import prediction
 
-# Load the trained model and scaler
-model = joblib.load('best_trained_model.pkl')
-scaler = joblib.load('best_standard_scaler.pkl')
+col1, col2 = st.columns([1, 5])
+# with col1:
+    # st.image("https://github.com/dicodingacademy/assets/raw/main/logo.png", width=130)
+with col2:
+    st.header('Student Status Prediction')
 
-# Function to make predictions
-def predict_status(inputs):
-    # Convert inputs to numpy array and reshape for the model
-    input_array = np.array(inputs).reshape(1, -1)
-    input_array = scaler.transform(input_array)  # Scaling the inputs
-    prediction = model.predict(input_array)  # Making the prediction
-    return prediction
+data = pd.DataFrame()
 
-# Streamlit UI setup
-st.title('Student Dropout Prediction')
+col1, col2, col3= st.columns(3)
 
-# Input fields for user data
-curricular_units_2nd_sem_approved = st.number_input('Curricular Units 2nd Semester Approved', min_value=0, max_value=30, value=15)
-curricular_units_2nd_sem_grade = st.number_input('Curricular Units 2nd Semester Grade', min_value=0, max_value=20, value=15)
-curricular_units_1st_sem_approved = st.number_input('Curricular Units 1st Semester Approved', min_value=0, max_value=30, value=15)
-curricular_units_1st_sem_grade = st.number_input('Curricular Units 1st Semester Grade', min_value=0, max_value=20, value=15)
-tuition_fees_up_to_date = st.selectbox('Tuition Fees Up to Date', [0, 1], format_func=lambda x: 'Yes' if x == 1 else 'No')
-scholarship_holder = st.selectbox('Scholarship Holder', [0, 1], format_func=lambda x: 'Yes' if x == 1 else 'No')
-curricular_units_2nd_sem_enrolled = st.number_input('Curricular Units 2nd Semester Enrolled', min_value=0, max_value=30, value=20)
-curricular_units_1st_sem_enrolled = st.number_input('Curricular Units 1st Semester Enrolled', min_value=0, max_value=30, value=20)
-admission_grade = st.slider('Admission Grade', min_value=0.0, max_value=200.0, value=5.0, step=0.1)
-displaced = st.selectbox('Displaced', [0, 1], format_func=lambda x: 'Yes' if x == 1 else 'No')
+with col1:
+    Application_order = int(st.number_input(label='Application_order', value=1))
+    data["Application_order"] = [Application_order]
 
-# Prepare input data for prediction
-input_data = [
-    curricular_units_2nd_sem_approved,
-    curricular_units_2nd_sem_grade,
-    curricular_units_1st_sem_approved,
-    curricular_units_1st_sem_grade,
-    tuition_fees_up_to_date,
-    scholarship_holder,
-    curricular_units_2nd_sem_enrolled,
-    curricular_units_1st_sem_enrolled,
-    admission_grade,
-    displaced
-]
+with col2:
+    Course = st.selectbox(label='Course', options=encoder_Course.classes_, index=1)
+    data["Course"] = [Course]
+    
+with col3:
+    Daytime_evening_attendance = st.selectbox(label='Daytime_evening_attendance', options=encoder_Daytime_evening_attendance.classes_, index=1)
+    data["Daytime_evening_attendance"] = [Daytime_evening_attendance]
 
-# Button for making prediction
+col1, col2, col3= st.columns(3)
+with col1:
+    Previous_qualification_grade = float(st.number_input(label='Previous_qualification_grade', value=140.0))
+    data["Previous_qualification_grade"] = [Previous_qualification_grade]
+with col2:
+    Mothers_occupation = st.selectbox(label='Mothers_occupation', options=encoder_Mothers_occupation.classes_, index=1)
+    data["Mothers_occupation"] = [Mothers_occupation]
+with col3:
+    Fathers_occupation = st.selectbox(label='Fathers_occupation', options=encoder_Fathers_occupation.classes_, index=1)
+    data["Fathers_occupation"] = [Fathers_occupation]
+
+col1, col2, col3= st.columns(3)
+with col1:
+    Admission_grade = float(st.number_input(label='Admission_grade', value=140.0))
+    data["Admission_grade"] = [Admission_grade]
+with col2:
+    Displaced = st.selectbox(label='Displaced', options=encoder_Displaced.classes_, index=1)
+    data["Displaced"] = [Displaced]
+with col3:
+    Tuition_fees_up_to_date = st.selectbox(label='Tuition_fees_up_to_date', options=encoder_Tuition_fees_up_to_date.classes_, index=1)
+    data["Tuition_fees_up_to_date"] = [Tuition_fees_up_to_date]
+
+col1, col2, col3= st.columns(3)
+with col1:
+    Scholarship_holder = st.selectbox(label='Scholarship_holder', options=encoder_Scholarship_holder.classes_, index=1)
+    data["Scholarship_holder"] = [Scholarship_holder]
+with col2:
+    International = st.selectbox(label='International', options=encoder_International.classes_, index=1)
+    data["International"] = [International]
+with col3:
+    Curricular_units_1st_sem_credited = int(st.number_input(label='Curricular_units_1st_sem_credited', value=2))
+    data["Curricular_units_1st_sem_credited"] = [Curricular_units_1st_sem_credited]
+
+col1, col2, col3= st.columns(3)
+with col1:
+    Curricular_units_1st_sem_enrolled = int(st.number_input(label='Curricular_units_1st_sem_enrolled', value=6))
+    data["Curricular_units_1st_sem_enrolled"] = [Curricular_units_1st_sem_enrolled]
+with col2:
+    Curricular_units_1st_sem_evaluations = float(st.number_input(label='Curricular_units_1st_sem_evaluations', value=11))
+    data["Curricular_units_1st_sem_evaluations"] = [Curricular_units_1st_sem_evaluations]
+with col3:
+    Curricular_units_1st_sem_approved = int(st.number_input(label='Curricular_units_1st_sem_approved', value=1))
+    data["Curricular_units_1st_sem_approved"] = [Curricular_units_1st_sem_approved]
+
+col1, col2, col3= st.columns(3)
+with col1:
+    Curricular_units_1st_sem_grade = float(st.number_input(label='Curricular_units_1st_sem_grade', value=11.833333))
+    data["Curricular_units_1st_sem_grade"] = [Curricular_units_1st_sem_grade]
+with col2:
+    Curricular_units_2nd_sem_credited = int(st.number_input(label='Curricular_units_2nd_sem_credited', value=2))
+    data["Curricular_units_2nd_sem_credited"] = [Curricular_units_2nd_sem_credited]
+with col3:
+    Curricular_units_2nd_sem_enrolled = int(st.number_input(label='Curricular_units_2nd_sem_enrolled', value=6))
+    data["Curricular_units_2nd_sem_enrolled"] = [Curricular_units_2nd_sem_enrolled]
+
+col1, col2, col3= st.columns(3)
+with col1:
+    Curricular_units_2nd_sem_evaluations = int(st.number_input(label='Curricular_units_2nd_sem_evaluations', value=1))
+    data["Curricular_units_2nd_sem_evaluations"] = [Curricular_units_2nd_sem_evaluations]
+with col2:
+    Curricular_units_2nd_sem_approved = int(st.number_input(label='Curricular_units_2nd_sem_approved', value=2))
+    data["Curricular_units_2nd_sem_approved"] = [Curricular_units_2nd_sem_approved]
+with col3:
+    Curricular_units_2nd_sem_grade = float(st.number_input(label='Curricular_units_2nd_sem_grade', value=11.857143))
+    data["Curricular_units_2nd_sem_grade"] = [Curricular_units_2nd_sem_grade]
+
+col1, col2 = st.columns(2)
+with col1:
+    Curricular_units_2nd_sem_without_evaluations = int(st.number_input(label='Curricular_units_2nd_sem_without_evaluations', value=6))
+    data["Curricular_units_2nd_sem_without_evaluations"] = [Curricular_units_2nd_sem_without_evaluations]
+    
+with col2:
+    GDP = float(st.number_input(label='GDP', value=1.79))
+    data["GDP"] = [GDP]
+
+with st.expander("View the Raw Data"):
+    st.dataframe(data=data, width=800, height=10)
+
 if st.button('Predict'):
-    prediction = predict_status(input_data)
-
-    # The prediction result is an array, so map the result to status
-    status_dict = {
-        0: 'Dropout',
-        1: 'Enrolled',
-        2: 'Graduate'
-    }
-    # Get the index of the highest predicted value
-    predicted_status_index = np.argmax(prediction, axis=1)[0]
-    predicted_status = status_dict[predicted_status_index]
-
-    st.write(f"The model predicts that the student is likely to be: **{predicted_status}**")
+    new_data = data_preprocessing(data=data)
+    with st.expander("View the Preprocessed Data"):
+        st.dataframe(data=new_data, width=800, height=10)
+    st.write("Student Status Prediction: {}".format(prediction(new_data)))
